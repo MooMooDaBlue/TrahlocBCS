c3a3f3c4d7835d227935766275f1125f27e199fe
MooMooDaBlue <cpapaino@gmail.com>
2015-01-22 03:50:39 -0700

    SCSS additions
    section dividers and so forth.

--- javascripts/custom.js
+++ javascripts/custom.js
@@ -1,7 +1,20 @@
 //FlexSlider JS
 
 $(window).load(function() {
-  $('.flexslider').flexslider({
+  $('.unit-slider').flexslider({
+    animation: "slide",
+    controlNav: "thumbnails"
+  });
+
+  $('.unit-slider1').flexslider({
+    animation: "slide",
+    controlNav: "thumbnails"
+  });
+  $('.unit-slider2').flexslider({
+    animation: "slide",
+    controlNav: "thumbnails"
+  });
+  $('.unit-slider3').flexslider({
     animation: "slide",
     controlNav: "thumbnails"
   });
